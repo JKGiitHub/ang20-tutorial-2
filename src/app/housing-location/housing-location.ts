3f3532f17b9d2df9cc4ee91fa5825cc2697f7507
Jk <johnrambox2@yahoo.com>
2025-07-14 18:01:28 +0300

    Les6: show info in HousingLocation component

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -5,7 +5,18 @@ import { HousingLocationInfo } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   imports: [],
-  templateUrl: './housing-location.html',
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation().photo"
+        alt="Exterior photo of {{ housingLocation().name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation().name }}</h2>
+      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
+    </section>
+  `,
   styleUrl: './housing-location.scss'
 })
 export class HousingLocation {
