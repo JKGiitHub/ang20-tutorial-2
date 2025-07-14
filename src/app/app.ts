d2a169e02322d52bdf660acafb038c4d03fec36e
Jk <johnrambox2@yahoo.com>
2025-07-14 19:25:03 +0300

    Les9p1: routing and empty Details component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
+import { RouterModule } from '@angular/router'; // for <a [routerLink]="['/']">  
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     Home,
+    RouterModule
   ],
   template: `
-    <main>      
-      <header class="brand-name">        
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />     
-      </header>      
-      <section class="content">        
-        <app-home></app-home>     
-      </section>   
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
     </main>
   `,
   styleUrl: './app.scss'
