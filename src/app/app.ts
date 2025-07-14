e696bf926fc0235959cbc6a793bfe570a3a5cfe8
Jk <johnrambox2@yahoo.com>
2025-07-14 15:59:57 +0300

    Les2: Homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Home,
+  ],
   template: `
-    <h1>Hello world!</h1>
+    <main>      
+      <header class="brand-name">        
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />     
+      </header>      
+      <section class="content">        
+        <app-home></app-home>     
+      </section>   
+    </main>
   `,
   styleUrl: './app.scss'
 })
